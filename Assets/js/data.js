bfaac081c280672115bc12ae1f780acbb64f0601
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-04-03 12:41:50 -0500

    Finished all of the script.js, organized, and commented. All functionality works as expected.

---
+++ Assets/js/data.js
@@ -0,0 +1,155 @@
+export const questions = [
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answerA: {
+      correct: false,
+      content: `<code>< scripting ></code>`
+    },
+    answerB: {
+      correct: true,
+      content: 'js'
+    },
+    answerC: {
+      correct: false,
+      content: 'javascript'
+    },
+    answerD: {
+      correct: false,
+      content: 'script'
+    },
+  },
+  {
+    question: 'What is the corrext JavaScript syntax to change the content of the HTML element below?\n\n<p id="demo">This is a demonstration.</p>',
+    answerA: {
+      correct: false,
+      content: 'document.getElement("p").innerHTML = "Hello World!";'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'Where is the correct place to insert a JavaScript?',
+    answerA: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What indicates a function?',
+    answerA: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What is a method?',
+    answerA: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What indicates a function?',
+    answerA: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What indicates a function?',
+    answerA: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What indicates a function?',
+    answerA: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+
+];
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,204 +1,474 @@
-const button = document.getElementById("landing-page-button")
-const landingPage = document.getElementById("landing-page")
+//Questions Data -- would like to move this to seperate file and import.
+const questions = [
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answerA: {
+      correct: false,
+      content: `<code>< scripting ></code>`
+    },
+    answerB: {
+      correct: true,
+      content: 'js'
+    },
+    answerC: {
+      correct: false,
+      content: 'javascript'
+    },
+    answerD: {
+      correct: false,
+      content: 'script'
+    },
+  },
+  {
+    question: 'What is the corrext JavaScript syntax to change the content of the HTML element below?\n\n<p id="demo">This is a demonstration.</p>',
+    answerA: {
+      correct: false,
+      content: 'document.getElement("p").innerHTML = "Hello World!";'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'Where is the correct place to insert a JavaScript?',
+    answerA: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What indicates a function?',
+    answerA: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What is a method?',
+    answerA: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What indicates a function?',
+    answerA: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What indicates a function?',
+    answerA: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+  {
+    question: 'What indicates a function?',
+    answerA: {
+      correct: true,
+      content: 'A function that is nested inside of an object'
+    },
+    answerB: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerC: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+    answerD: {
+      correct: false,
+      content: 'A function that is nested inside of an object'
+    },
+  },
+
+];
+//assign Glogbal variables. --> needs to be cleaned up, some need to be taken away and some need to be added
 const gameStatsArticle = document.getElementById("game-stats");
 const gameTimer = document.getElementById("game-timer");
+const landingPage = document.getElementById("landing-page")
+const highscoresPage = document.getElementById('highscores-page')
+const highscoresPageButton = document.getElementById('highscores-page-btn')
 const questionsBox = document.getElementById("questions-box");
-const answerForm = document.getElementById("answer-form");
+const landingButton = document.getElementById("landing-page-button")
 const startButton = document.getElementById("start-button");
-let answerInput = document.getElementById("answer-input");
-let scoreEl = document.getElementById('score');
+const answerButton = document.querySelector("#answer-button");
+const clearButton = document.getElementById("clear-history");
+const scoreInput = document.getElementById("score-input");
+const alertEl = document.getElementById("answer-alert");
+  
+let questionCounter = 0; //set a question counter to 0
+let time = 30; //30 seconds on the clock to start
+let timer; //a timer placeholder that will hold the 1 second interval and countDown functions.
 
-const enterSite = () => {
-  console.info("Entering Site!");
-  landingPage.style.display = "hidden";
-};
-
-//? 1. Initialize local storage
+//initialize local storage
 //get gameStats from local storage
 const getGameStatsData = () => {
   const gameStats = JSON.parse(localStorage.getItem('gameStats'));
   //if gameStats is null return an empty array otherwise return the contents of 'gameStats'
   return gameStats === null ? [] : gameStats;
 };
+
 //initialize the gameStats data
 let gameStatsData = getGameStatsData();
 
-console.info(gameStatsData);
+//function that handles dynamically setting the question in the DOM based on the questions in the questions array passing in the questionCounter to update the questions by index using bracket notation.
+const setQuestion = () => {
+  //if we even have questions in our questions array. if there is even data.
+  if (questions) {
+    return (
+      //update the innerHTML to the selected question and answer object.
+      questionsBox.innerHTML = `
+      <h3>${questions[questionCounter].question}</h3>
+      <ul>
+        <li>
+          <button 
+            id="answer-button" 
+            class="answer-btn" 
+            data-correct="${questions[questionCounter].answerA.correct}" 
+            onclick="handleAnswer(event)"
+          >
+            1. ${questions[questionCounter].answerA.content}
+          </button>
+        </li>
+        <li>
+          <button 
+            id="answer-button" 
+            class="answer-btn" 
+            data-correct="${questions[questionCounter].answerB.correct}" 
+            onclick="handleAnswer(event)"
+          >
+            2. ${questions[questionCounter].answerB.content}
+          </button>
+        </li>
+        <li>
+          <button 
+            id="answer-button" 
+            class="answer-btn" 
+            data-correct="${questions[questionCounter].answerC.correct}" 
+            onclick="handleAnswer(event)"
+          >
+            3. ${questions[questionCounter].answerC.content}
+          </button>
+        </li>
+        <li>
+          <button 
+            id="answer-button" 
+            class="answer-btn" 
+            data-correct="${questions[questionCounter].answerD.correct}" 
+            onclick="handleAnswer(event)"
+          >
+            4. ${questions[questionCounter].answerD.content}
+          </button>
+        </li>
+      </ul>
+      `
+      );
+  }
+};
 
+//set the game stats data in the DOM
 const setGameStats = () => {
-  gameStatsData.length == 0 ?
+  gameStatsData.length == 0 ? //if there is no gameStatsData yet then return the following
   gameStatsArticle.innerHTML = `
     <p>No games have been played yet. Stats will be added after the first game.</p>
-  ` :
+  ` : //if there is gameStatsData then show the game stats board
   gameStatsArticle.innerHTML = `
-  <h3>Game Statistics</h3>
-  <h5>Games Played: ${gameStatsData.length}</h5>
   <table>
     <tr>
-      <th>Game #</th>
-      <th>Result</th>
+      <th>Initials</th>
       <th>Score</th>
-      <th>Accuracy</th>
-      <th>Time Played</th>
     </tr>
-    ${gameStatsData.map(stats => `
+    ${//loop through every game thats been played and make a row in the table from it
+      gameStatsData.map(stats => ` 
       <tr>
-        <th>${stats.gameNo}</th>
-        <th>${stats.result}</th>
-        <th>${stats.score}</th>
-        <th>${stats.percentCorrect}</th>
-        <th>${stats.minutesPlayed + `:` + stats.seconds}</th>
+        <td>${stats.initials}</td>
+        <td>${stats.score}</td>
       </tr>`
       )}
   </table>
   `;
 };
 
-
+//we want to run this on initialization so the data is present before the user.
 setGameStats();
-console.info(gameStatsData);
-  
-let questionCounter = 0;
-let time = 30; //600 seconds = 10 minutes
-let score = 0;
-let timer; //a timer placeholder that will hold the 1 second interval and countDown functions.
-
-gameTimer.innerHTML = (time / 60).toFixed(2); //set default starting time to 10 minutes.
-
-const setScore = () => {
-  scoreEl.innerHTML = score; //set default score to 0.
-};
-
-setScore();
-
-const questions = [
-  {
-    question: 'What is a method?',
-    answer: 'A function that is nested inside of an object'
-  },
-  {
-    question: 'What is a function?',
-    answer: 'a'
-  },
-  {
-    question: 'What does HTML stand for?',
-    answer: 'b'
-  }
-];
-
-console.log(answerInput)
-
-const setQuestion = () => {
-  if (questions) {
-    questionsBox.innerHTML = `
-      <div>
-        <p>Question: ${questions[questionCounter].question}</p>
-      </div>
-    `;
-  }
-};
-
+//set a question on initialization so that there is already a question and answer set in the DOM before the user.
 setQuestion();
 
-const endGame = () => {
-  const percentCorrect = score / questionCounter;
+// -- Helpers --
+// alertHandler(correct) -- handles the correct or wrong! alert based on user answer. pass in a boolean.
+// checkCounters() -- compares the questionCounter against the length of questions left.
+// handleAnswer() -- checks if the answer is correct or incorrect and adjusts score and time accordingly. 
+// handleLocalStorage(initials) -- sets the model for the data and stores the game stats object in local storage, takes in the initials captured from the final end game initials form.
+// endGame() -- handles the behavior when the game ends.
+// countDown() -- handles the count down of the timer.
+// startTimer() -- starts the timer.
+// resetTimer() -- ends and resets the clock/timer.
+// switchPage(page) -- handles routing and switching between pages. takes in a string directing to which page. ex. "toLanding", "toMain", etc...
 
+//function that handles alerting when the user answers right or wrong.
+const alertHandler = (correct) => {
+  //if correct
+  if (correct) {
+    //update the innerHTML 
+    alertEl.innerHTML = `
+      <hr />
+      <h2 class="alert-title">"Correct!"</h2>
+    `;
+    //change background to lightgreen
+    alertEl.style.backgroundColor = "lightgreen";
+    //and show it
+    alertEl.style.display = "block";
+    //then after 3 seconds
+    setTimeout(() => {
+      //make it disappear
+      alertEl.style.display = "none";
+    }, 3000);
+  }
+  //otherwise if the user is incorrect.
+  else { 
+    //update the innerHTML
+    alertEl.innerHTML = `
+      <hr />
+      <h2 class="alert-title">"Incorrect!"</h2>
+    `;
+    //change the background to red.
+    alertEl.style.backgroundColor = "red";
+    //and show it
+    alertEl.style.display = "block";
+    //after 3 seconds
+    setTimeout(() => {
+      //make it disappear
+      alertEl.style.display = "none";
+    }, 3000);
+  }
+};
+// function to check the questionCounter and questions.length to look for a condition to end the game.
+const checkCounters = () => {
+  //if the questionCounter is equal to or somehow greater than questions.length the game will end.
+  if ( questionCounter >= questions.length ) {
+    alert("Congratulation's, you have won! confetti confetti.");
+    endGame();
+  }
+};
+
+//function that handles each answer given.
+const handleAnswer = (event) => {
+
+  //check the counters to see if we should end the game.
+  checkCounters();
+
+  //assign a shorter variable for the data being passed by the dataset attribute.
+  const isCorrect = event.target.dataset.correct;
+  //if the answer is correct.
+  if (isCorrect === "true") {
+    //fire the alert handler passing in true.
+    alertHandler(true);
+    //increase the questionCounter by 1.
+    questionCounter += 1;
+    //give the user a bonus time boost.
+    time += 15;
+    //reset the question in the DOM.
+    setQuestion();
+  } 
+  //if the answer is incorrect.
+  else { 
+    //fire the alertHandler passing in false.
+    alertHandler(false);
+    //increment the questionCounter.
+    questionCounter += 1;
+    //penalize the user with -5 seconds on the clock.
+    time -= 5;
+    //reset the question in the DOM.
+    setQuestion();
+  }
+};
+
+//wrapped localStorage handling in function passing in initials captured at end of game flow.
+const handleLocalStorage = (initials) => {
+  //build a data model
   let stats = {
     message: "",
-    numberOfQuestions: questionCounter,
-    score: score,
-    percentCorrect: percentCorrect,
-    minutesPlayed: Math.floor(time / 60),
-    seconds: time % 60
+    initials: initials,
+    score: time,
   };
 
-  if (percentCorrect > 0.8) {
-    alert("Congratulation's, you have won! confetti confetti.");
-    stats.message = "You won!";
-    gameStatsData.push(stats);
-    console.info(gameStatsData);
-    try {
-      localStorage.setItem('gameStats', JSON.stringify(gameStatsData));
-      setGameStats();
-    } catch (exception) {
-      console.error(exception);
-    }
-
-    console.info(JSON.parse(localStorage.getItem('gameStats')));
-  } 
-  else {
-    alert("Sorry, practice a little more and try again.");
-    stats.message = "You lost!";
-    gameStatsData.push(stats);
-    console.info(gameStatsData);
-    try {
-      localStorage.setItem('gameStats', JSON.stringify(gameStatsData));
-      setGameStats();
-    } catch (exception) {
-      console.error(exception);
-    }
+  //if time > 0 then set stats.message to "You won!" otherwise set it to "You lost!"
+  time > 0 ? stats.message = "You won!" : stats.message = "You lost!";
+  //push the stats object from this game to the stats array saved in localStorage.
+  gameStatsData.push(stats);
+  //add trycatch block for error handling.
+  try {
+    //set item in local storage formatting for JSON.
+    localStorage.setItem('gameStats', JSON.stringify(gameStatsData));
+    //set updated gameStats to the DOM.
+    setGameStats();
+  } catch (exception) {
+    //if error catch and display error
+    console.error(exception);
   }
 
-  resetTimer();
-  setScore();
+  //reset question counter
+  questionCounter = 0;
+  //reset the clock back to 30 seconds for the next game.
+  time = 30;
+  //reset score for next game.
   score = 0;
-  return score;
-  
+  //switch pages
+  switchPage("toHighScores");
 };
 
+//function that handles the count down of the clock.
 const countDown = () => { 
-  if ( time <= 0) { endGame(); }
-  time--; 
-  let minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-  let formattedTime = minutes + ':' + seconds;
-  gameTimer.innerHTML = formattedTime;
-};
-
-const startTimer = () => {
-  if (time > 0) { timer = setInterval(countDown, 1000); }
-  else { endGame(); }
-};
-
-const resetTimer = () => {
-  clearInterval(timer);
-  time = 600;
-};
-
-const checkAnswer = (event) => {
-  event.preventDefault();
-  if (answerInput.value.trim() === questions[questionCounter].answer) {
-    score ++;
-    answerInput.value = '';
-    questionCounter++;
-    setScore();
-    setQuestion();
-    console.info(questionCounter, questions.length);
-    if (questionCounter === questions.length) { endGame(); }
-  } else {
-    answerInput.value = '';
-    questionCounter++;
-    setScore();
-    setQuestion();
-    console.info(questionCounter, questions.length);
-    if (questionCounter === questions.length) { endGame(); }
+  //if time is less than or equal to 0 then end the game.
+  if (time <= 0) { 
+    //stops the countdown and resets the timer.
+    clearInterval(timer); 
+    endGame(); 
+  }
+  else {
+    //otherwise decrement time by 1.
+    time--; 
+    gameTimer.innerHTML = time; //setTime()
   }
 };
 
+// function that handles starting the timer when called
+const startTimer = () => { timer = setInterval(countDown, 1000); };
+
+//function that handles the behavior when the game ends.
+const endGame = () => {
+  
+  scoreInput.value = time;
+  //switch pages
+  switchPage("toEndGame");
+  return;
+};
+
+//special switch case function to handle switching the pages.
+const switchPage = (page) => {
+  switch (page) {
+    case "toMain":
+      //switch from landing page to main page
+      document.querySelector("#landing-page").style.display = "none";
+      document.getElementById("main-section").style.display = "block";
+      break;
+    case "toLanding":
+      //switch from highscores page or endGame page to landing page
+      document.getElementById("end-game-page").style.display = "none";
+      document.querySelector("#highscores-page").style.display = "none";
+      document.querySelector("#landing-page").style.display = "flex";
+      break;
+    case "toHighScores":
+      //switch from main page or endGame page to highscores page
+      document.getElementById("end-game-page").style.display = "none";
+      document.getElementById("main-section").style.display = "none";
+      document.querySelector("#highscores-page").style.display = "block";
+      break;
+    case "toEndGame":
+      //switch from main page to end page
+      document.getElementById("main-section").style.display = "none";
+      document.getElementById("end-game-page").style.display = "block";
+      break;
+    default:
+      null;
+  }
+}
 
 //Event Listeners
-button.addEventListener("click", {
-
+landingButton.addEventListener("click", () => {
+  startTimer();
+  switchPage("toMain");
 });
 
 startButton.addEventListener("click", () => { 
-  console.info("Timer is started!"); 
-  startTimer();
-  document.getElementById("main-section").style.display = "block";
+  switchPage("toLanding");
 });
 
-answerForm.addEventListener("submit", event => {
+highscoresPageButton.addEventListener("click", () => {
+  switchPage("toHighScores");
+});
+
+document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-  console.info("I have been clicked.", event);
-  checkAnswer(event);
+  const initials = document.querySelector("#initials-input").value;
+  initials && handleLocalStorage(initials);
+})
+
+clearButton.addEventListener("click", () => { 
+  if (confirm("Are you sure you want to clear the game history?")) {
+    localStorage.clear('gameStats');
+  }
+  window.location.reload();
 });
